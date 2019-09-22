152282a5ccf9ed1d95175595081e47edfa0f10d1
tim@pop-os <tim@pop-os>
2019-09-22 11:33:04 -0700

    Add program map_rdf_file.py.

---
+++ scripts/map_rdf_file.py
@@ -0,0 +1,105 @@
+
+from __future__ import print_function
+
+import argparse
+import gc
+import json
+import sys
+import time
+
+from collections import defaultdict
+from io import BytesIO
+from os.path import isdir, isfile, join
+from rdflib.plugins.parsers.ntriples import NTriplesParser, ParseError, Sink
+
+from bdb_tool import open_db, db_nkeys, BdbUniqueId
+
+
+def parse_args():
+    desc = 'Read RDF file and create a set of Berkeley DB files to describe it, ' \
+           'to enable querying it in a memory-constrained environment.'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('inpath', help='path to input file')
+    parser.add_argument('outpath', help='path to output dir')
+    return parser.parse_args()
+
+
+class ElementStrError(Exception):
+    pass
+
+
+def element_str(element):
+    ''' Return string representation for rdf element. '''
+    try:
+        return str(element).encode('unicode_escape')
+    except UnicodeEncodeError:
+        try:
+            return element.toPython().encode('unicode_escape')
+        except Exception as e:
+            print(e.__class__.__name__, ':', e.args[0])
+            print('type(element) =', type(element))
+            raise ElementStrError()
+
+
+class BdbSink(Sink):
+    def __init__(self, outpath):
+        self.nlines = 0
+        self.nouns = BdbUniqueId(outpath, 'noun', log=print)
+        self.verbs = BdbUniqueId(outpath, 'verb', log=print)
+        self.edges = open_db(join(outpath, 'edges.db3'))
+        print('number of edges is', db_nkeys(self.edges, 'edges'))
+        self.progress_time = self.last_gc_time = time.time()
+
+    def progress(self, count):
+        now = time.time()
+        if now < self.progress_time + 0.1:
+            return
+        self.progress_time = now
+        sys.stderr.write('progress: {}    \r'.format(count))
+
+    def triple(self, s, p, o):
+        self.nlines += 1
+        sid = self.nouns.element_id(element_str(s))
+        pid = self.verbs.element_id(element_str(p))
+        oid = self.nouns.element_id(element_str(o))
+        s_edges = set([tuple(edge) for edge in json.loads(self.edges.get(sid) or '[]')])
+        s_edge = (pid.decode('ascii'), oid.decode('ascii'))
+        if s_edge not in s_edges:
+            s_edges.add(s_edge)
+            self.edges.put(sid, json.dumps(list(s_edges)))
+        now = time.time()
+        if now - self.last_gc_time > 100:
+            print('gc', now)
+            gc.collect()
+            self.last_gc_time = now
+        self.progress(self.nlines)
+
+
+def process_file(infile, sink):
+    bad_lines = defaultdict(int)
+    for line in infile:
+        s = BytesIO()
+        s.write(line)
+        s.seek(0)
+        parser = NTriplesParser(sink)
+        try:
+            parser.parse(s)
+        except (ParseError, ElementStrError) as e:
+            bad_lines[line] += 1
+    print('read {} lines from {}'.format(sink.nlines, infile.name))
+    print('bad lines and their frequencies:')
+    for line, count in bad_lines.items():
+        print('  {:>10} : {}'.format(count, line))
+
+
+def main():
+    args = parse_args()
+    if not isfile(args.inpath):
+        sys.exit('E: no such file {}'.format(args.inpath))
+    if not isdir(args.outpath):
+        sys.exit('E: no such dir {}'.format(args.outpath))
+    process_file(open(args.inpath, 'rb'), BdbSink(args.outpath))
+
+
+if __name__ == '__main__':
+    main()
