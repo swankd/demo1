a17c201cf053d0deae414ad4b63be3fb2a5ac289
tim@pop-os <tim@pop-os>
2019-09-29 20:28:20 -0700

    rdf_demo: add /noun/ and /verb/ routes.

--- rdf_demo/rdf_demo/views.py
+++ rdf_demo/rdf_demo/views.py
@@ -1,8 +1,17 @@
 
+from flask import request
+
 from .app import app
 from .db import DB
 
 db = DB()
+
+
+def request_args(name, func, default):
+    try:
+        return func(request.args.get(name, default))
+    except (ValueError, TypeError):
+        return None
 
 
 @app.route('/')
@@ -10,3 +19,15 @@ def root():
     return '<br>\n'.join([f'{db.nnouns} nouns',
                           f'{db.nverbs} verbs',
                           f'{db.nedges} edges'])
+
+
+@app.route('/noun/')
+def nouns():
+    nitems = request_args('nitems', int, 20)
+    return '<br>\n'.join([f'{key} : {value}' for key, value in db.nouns.items()[:nitems]])
+
+
+@app.route('/verb/')
+def verbs():
+    nitems = request_args('nitems', int, 20)
+    return '<br>\n'.join([f'{key} : {value}' for key, value in db.verbs.items()[:nitems]])
