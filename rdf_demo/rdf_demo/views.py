c6ee35ee72c514e9d679df80ba68b0b62516f4e0
tim@pop-os <tim@pop-os>
2019-10-08 21:27:19 -0700

    rdf_demo: add /noun/<id_> route.

--- rdf_demo/rdf_demo/views.py
+++ rdf_demo/rdf_demo/views.py
@@ -1,5 +1,7 @@
 
-from flask import request
+import json
+
+from flask import jsonify, request, url_for
 
 from .app import app
 from .db import BdbRDFGraph
@@ -12,6 +14,26 @@ def request_args(name, func, default):
         return func(request.args.get(name, default))
     except (ValueError, TypeError):
         return None
+
+
+def noun_reference(id_):
+    return {'value': graph.nouns.element_str(id_), 'href': url_for('noun', id_=id_)}
+
+
+def verb_reference(id_):
+    return graph.verbs.element_str(id_)
+
+
+def in_edge(pid, sid):
+    return {'predicate': verb_reference(pid), 'subject': noun_reference(sid)}
+
+
+def out_edge(pid, oid):
+    return {'predicate': verb_reference(pid), 'object': noun_reference(oid)}
+
+
+def _edges(db_, id_):
+    return set([tuple(edge) for edge in json.loads(db_.get(id_.encode('ascii')) or '[]')])
 
 
 @app.route('/')
@@ -34,3 +56,12 @@ def verbs():
     nitems = request_args('nitems', int, 20)
     return '<br>\n'.join([f'{key} : {value}'
                           for key, value in graph.verbs.items()[:nitems]])
+
+
+@app.route('/noun/<id_>')
+def noun(id_):
+    return jsonify({'noun': noun_reference(id_),
+                    'in_edges': [in_edge(pid, sid)
+                                 for pid, sid in _edges(graph.in_edges, id_)],
+                    'out_edges': [out_edge(pid, oid)
+                                  for pid, oid in _edges(graph.out_edges, id_)]})
