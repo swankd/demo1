c805995b1ff131002310cc2a0d87af94bad6d71a
tim@pop-os <tim@pop-os>
2019-10-29 07:38:53 -0700

    git_db.py: introduce find_in_page function.

--- git_db/git_db/git_db.py
+++ git_db/git_db/git_db.py
@@ -4,6 +4,76 @@ import json
 from pygit2 import GitError, Repository, init_repository, GIT_OBJ_BLOB
 
 
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+class BadPage(Exception):
+    pass
+
+
+class BadPageSize(BadPage):
+    pass
+
+
+class BadWidth(ValueError):
+    pass
+
+
+def find_in_page(page, oid, width):
+    '''
+    Binary search for ``oid`` in ``page``.  ``width`` may be 18 or 38.  len(``oid``) must
+    be 18.  len(``page``) must be multiple of ``width``.
+
+    Return value is None if ``oid`` is not found in ``page``.
+
+    If ``oid`` is found, return value is the 20 bytes following the occurrence of ``oid``
+    in ``page`` if ``width`` is 38, or True if ``width`` is 18.
+    '''
+    def compare_entry(k):
+        pos = k * width
+        entry = page[pos:pos + width]
+        result = cmp(oid, entry[:18])
+        if result == 0 and width == 38:
+            return entry[18:]
+        else:
+            return result
+
+    if width not in (18, 38):
+        raise BadWidth(width)
+
+    if len(page) % width:
+        raise BadPageSize((len(page), width))
+
+    nentries = len(page) // width
+    k0 = 0
+    k1 = nentries - 1
+    while True:
+        assert k1 >= k0
+        kmid = (k0 + k1) // 2
+        result = compare_entry(kmid)
+        if result == 0 or isinstance(result, bytes):
+            break
+
+        if k0 == k1:
+            break
+        if k1 - k0 == 1:
+            result = compare_entry(k1)
+            break
+
+        if result < 0:
+            k1 = kmid - 1
+        else:
+            k0 = kmid + 1
+
+    if result == 0:
+        return True
+    elif isinstance(result, bytes):
+        return result
+    else:
+        return None
+
+
 class GitDict():
     def __init__(self, dir_, name, log=None, do_create=False, refs_ns='tags'):
         self.dir_ = dir_
--- git_db/tests/test_git_db.py
+++ git_db/tests/test_git_db.py
@@ -1,8 +1,10 @@
 
+import random
 import tempfile
 import unittest
 
 from git_db import GitDict
+from git_db.git_db import find_in_page
 
 
 class TestGitDict(unittest.TestCase):
@@ -27,3 +29,42 @@ class TestGitDict(unittest.TestCase):
             self.assertIn('abc', gd)
             self.assertIn('def', gd)
             self.assertEqual(len(gd), 3)
+
+
+class Test_find_in_page(unittest.TestCase):
+    def make_page_entry(self, width):
+        return bytes([random.randint(97, 122) for x in range(width)])
+
+    def make_page(self, width, nentries, include_entry=None):
+        if include_entry:
+            assert len(include_entry) == width, (include_entry, width)
+        entries = [self.make_page_entry(width) for _ in range(nentries)]
+        if include_entry:
+            entries += [include_entry]
+        for entry in sorted(entries):
+            print(entry)
+        return b''.join(sorted(entries))
+
+    def test18_missing(self):
+        for _ in range(10):
+            entry = self.make_page_entry(18)
+            page = self.make_page(18, random.randint(1, 20))
+            self.assertIsNone(find_in_page(page, entry, 18))
+
+    def test18_present(self):
+        for _ in range(100):
+            entry = self.make_page_entry(18)
+            page = self.make_page(18, random.randint(1, 20), entry)
+            self.assertTrue(find_in_page(page, entry, 18))
+
+    def test38_missing(self):
+        for _ in range(10):
+            entry = self.make_page_entry(38)
+            page = self.make_page(38, random.randint(1, 20))
+            self.assertIsNone(find_in_page(page, entry[:18], 38))
+
+    def test38_present(self):
+        for _ in range(100):
+            entry = self.make_page_entry(38)
+            page = self.make_page(38, random.randint(1, 20), entry)
+            self.assertEqual(find_in_page(page, entry[:18], 38), entry[18:])
