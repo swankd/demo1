baa3eff04411bd5efc926ef33c7bf8d456392b9e
tim@pop-os <tim@pop-os>
2019-11-12 19:23:38 -0800

    demo_siege.py: report response statistics.

--- scripts/demo_siege.py
+++ scripts/demo_siege.py
@@ -2,6 +2,9 @@
 import argparse
 import random
 import requests
+import time
+
+from collections import defaultdict
 
 ROUTES = ('subject', 'predicate', 'object')
 
@@ -14,6 +17,27 @@ def parse_args():
     return parser.parse_args()
 
 
+class Reporter():
+    def __init__(self):
+        self.last_time = time.time()
+        self.nchars = self.nrequests = 0
+        self.status_codes = defaultdict(int)
+
+    def add(self, response):
+        self.nchars += len(response.text)
+        self.nrequests += 1
+        self.status_codes[response.status_code] += 1
+        dt = time.time() - self.last_time
+        if dt > 10:
+            kb_per_second = self.nchars / (1000 * dt)
+            requests_per_second = self.nrequests / dt
+            status_codes = {code: count for code, count in self.status_codes.items()}
+            print(f'nchars={self.nchars} nrequests={self.nrequests} '
+                  f'kbps={kb_per_second:.1f} rps={requests_per_second:.1f} ')
+            print(f'... status_codes={status_codes}')
+            self.__init__()
+
+
 def main(args):
     assert args.url.endswith('/'), args.url
 
@@ -22,6 +46,7 @@ def main(args):
 
     max_ids = {route: _max_id(route) for route in ROUTES}
     print('max_ids:', max_ids)
+    reporter = Reporter()
 
     while True:
         route = random.choice(ROUTES)
@@ -32,7 +57,7 @@ def main(args):
             offset = str(random.randint(-1, int(max_ids[route] * 1.1)))
             id_ = f'?limit={limit}&offset={offset}'
 
-        requests.get(f'{args.url}{route}/{id_}')
+        reporter.add(requests.get(f'{args.url}{route}/{id_}'))
 
 
 if __name__ == '__main__':
